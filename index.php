e5dcf8b4b623d4f6d451c2ab7ec16fcaa1a1cbc6
ssamu801 <maezy_manuel@dlsu.edu.ph>
2023-07-29 02:47:46 +0800

    index

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head> <
+    </head>
+    <body>
+        <input type="text" name="username" placeholder="Username"><br>
+        <input type="password" name="password" placeholder="Password"><br>
+        <button type="submit" name="login"> LOGIN </button>
+    </body>
+</html>
